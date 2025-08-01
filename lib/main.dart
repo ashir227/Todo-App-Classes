b6a13dac4075c230d5d4b4574bdd4747950249d1
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-01 22:48:50 +0500

    RUN

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp()); // Entry point of the app
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Removes debug banner
       home: Scaffold(
+        appBar: AppBar(title: const Text('Todo App')),
         body: ListView(
-          children: [
+          padding: const EdgeInsets.all(16.0),
+          children: const [
             Text(
-              "One ",
+              "One",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
+            SizedBox(height: 10),
             Text(
-              "One ",
+              "Two",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
+            SizedBox(height: 10),
             Text(
-              "One ",
+              "Three",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
+            SizedBox(height: 10),
             Text(
-              "One ",
+              "Four",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
             ),
           ],
