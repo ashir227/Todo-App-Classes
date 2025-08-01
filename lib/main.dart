ea1ed68028b257df8ac88bf995930da4b3cecf1b
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-01 23:10:28 +0500

    Repo

--- lib/main.dart
+++ lib/main.dart
@@ -13,29 +13,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // Removes debug banner
       home: Scaffold(
         appBar: AppBar(title: const Text('Todo App')),
-        body: ListView(
-          padding: const EdgeInsets.all(16.0),
-          children: const [
-            Text(
-              "One",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Two",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Three",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(height: 10),
-            Text(
-              "Four",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-          ],
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Text("aa", style: TextStyle(fontSize: 20));
+          },
+          itemCount: 25,
+          itemExtent: 100,
         ),
       ),
     );
