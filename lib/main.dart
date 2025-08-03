f26cf174a10a743eaa1e39df0bb747ec91e3e2cd
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-03 12:57:17 +0500

    Todo App

--- lib/main.dart
+++ lib/main.dart
@@ -4,45 +4,21 @@ void main() {
   runApp(const MyApp()); // Entry point of the app
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    var data = [
-      "Ashir",
-      "Bilal,",
-      "Mehmood",
-      "Faisal",
-      "Moon",
-      "Anas",
-      "daniyal",
-      "Mohsin",
-      "umar",
-      "Noman",
-    ];
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Removes debug banner
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Todo App'),
-          backgroundColor: Colors.grey,
-        ),
-        body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Container(
-              color: Colors.blue,
-              padding: EdgeInsets.only(bottom: -0),
-              margin: EdgeInsets.only(bottom: 12),
-              child: Text(data[index], style: TextStyle(fontSize: 20)),
-              // color: Colors.yellow,
-              height: 30,
-            );
-          },
-          itemCount: data.length,
-          separatorBuilder: (context, index) {
-            return Divider(height: 100, thickness: 4);
-          },
+          backgroundColor: Colors.black,
+          title: Text("Todo App !", style: TextStyle(color: Colors.white)),
         ),
       ),
     );
