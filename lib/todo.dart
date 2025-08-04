6505f847b2656602bb35895d8100ec7d29ca4d7b
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-04 23:06:25 +0500

    ui todoapp

--- lib/todo.dart
+++ lib/todo.dart
@@ -9,30 +9,45 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+  List myfile = ["Ashir"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 54, 11, 228),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(80),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(219, 7, 10, 226),
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(130),
+                  ),
                 ),
-              ),
-              width: double.infinity,
-              height: 500,
 
-              child: Container(
-                color: Colors.amber,
-                height: 800,
-                width: 100,
+                height: 250,
+                width: double.infinity,
                 child: Column(),
               ),
-            ),
-          ],
+              Container(
+                color: Colors.blueGrey,
+                height: 600,
+                // padding: EdgeInsets.only(bottom: 200),
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 30),
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text("Careers Advice"),
+                      subtitle: Text("Ashu Baba"),
+                      trailing: Icon(Icons.check_box_outline_blank),
+                    );
+                  },
+                ),
+              ),
+              FloatingActionButton(onPressed: () {}),
+            ],
+          ),
         ),
       ),
     );
