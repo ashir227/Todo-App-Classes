2cd98c7836d327968fb0ce1754287d8e6a4e1e21
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-06 20:46:13 +0500

    ui Updates

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,105 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+// import 'package:TodoApp/main.dart';
 
 class TodoApp extends StatefulWidget {
   const TodoApp({super.key});
 
   @override
-  State<TodoApp> createState() => _TodoPageState();
+  State<TodoApp> createState() => _TodoAppState();
 }
 
-class _TodoPageState extends State<TodoApp> {
-  List<Map<String, dynamic>> myfile = [];
-  TextEditingController mycon = TextEditingController();
-
+class _TodoAppState extends State<TodoApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(),
-      body: buildListView(),
-      bottomNavigationBar: buildFooter(context),
-    );
-  }
-
-  // 🔷 App Bar
-  AppBar buildAppBar() {
-    return AppBar(title: Text("Todo"), centerTitle: true);
-  }
-
-  // 🔷 ListView for showing todo items
-  Widget buildListView() {
-    return ListView.builder(
-      padding: EdgeInsets.only(bottom: 100),
-      itemCount: myfile.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(
-            myfile[index]["title"],
-            style: TextStyle(
-              decoration: myfile[index]["done"]
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none,
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: RichText(
+          text: TextSpan(
+            children: [TextSpan(text: "Tech", style: TextStyle(fontSize: 30))],
           ),
-          trailing: IconButton(
-            onPressed: () {
-              toggleDone(index);
-            },
-            icon: Icon(
-              myfile[index]["done"]
-                  ? Icons.check_box
-                  : Icons.check_box_outline_blank,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  // 🔷 Footer: TextField + Add Button
-  Widget buildFooter(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).viewInsets.bottom + 12,
-        left: 10,
-        right: 10,
-        top: 10,
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: mycon,
-              decoration: InputDecoration(
-                hintText: "Enter task...",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            mini: true,
-            onPressed: addTodoItem,
-            child: Icon(Icons.add),
-          ),
-        ],
+        ),
       ),
     );
-  }
-
-  // 🔷 Logic: Add new todo
-  void addTodoItem() {
-    if (mycon.text.isNotEmpty) {
-      setState(() {
-        myfile.add({"title": mycon.text, "done": false});
-        mycon.clear();
-      });
-    }
-  }
-
-  // 🔷 Logic: Toggle checkbox
-  void toggleDone(int index) {
-    setState(() {
-      myfile[index]["done"] = !myfile[index]["done"];
-    });
   }
 }
