a548643036da1c8b44e9eee501a709102eda47c3
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-10 10:29:00 +0500

    color change6

--- lib/todo.dart
+++ lib/todo.dart
@@ -32,7 +32,7 @@ class _TodoAppState extends State<TodoApp> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        color: const Color.fromARGB(255, 44, 197, 214),
+        color: const Color.fromARGB(255, 174, 214, 44),
         child: Column(
           children: [
             Expanded(
