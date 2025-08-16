75af651e49334b26e6a4e68df1554566c20d2e93
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-16 21:41:53 +0500

    new Function

--- lib/screens/NextScrn.dart
+++ lib/screens/NextScrn.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
 class NextScr extends StatelessWidget {
-  const NextScr({super.key});
+  final Map stddata;
+  const NextScr({super.key, required this.stddata});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("NextGen")),
-      body: ElevatedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text("Nextscreen"),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Nextscreen"),
+          ),
+          Text(stddata["name"]),
+          Text(stddata["course"]),
+          Text(stddata["sec"]),
+        ],
       ),
     );
   }
