8b7ca120a14e29467550fec81ee6dcef5a0d88aa
Ashir Ahmed <ashirahmed810@gmail.com>
2025-10-08 20:26:30 +0500

    UI Prac

--- lib/UI Prac/Screen.dart
+++ lib/UI Prac/Screen.dart
@@ -11,13 +11,35 @@ class _ScreensState extends State<Screens> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.redAccent, title: Text("ddd")),
       body: Container(
         height: double.infinity,
         width: double.infinity,
-        color: Colors.amber,
-        child: Column(children: [
-          
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text.rich(
+              TextSpan(
+                text: "health",
+
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.w600,
+                ),
+                children: [
+                  TextSpan(
+                    text: "cabal",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login"),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+            ),
           ],
         ),
       ),
