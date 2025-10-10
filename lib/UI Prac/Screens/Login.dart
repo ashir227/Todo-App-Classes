fb14721d164810abccbef65adbf40d182fcffbd8
Ashir Ahmed <ashirahmed810@gmail.com>
2025-10-10 19:52:17 +0500

    UI Prac

--- lib/UI Prac/Screens/Login.dart
+++ lib/UI Prac/Screens/Login.dart
@@ -16,35 +16,55 @@ class _LoginscrState extends State<Loginscr> {
         padding: EdgeInsets.only(top: 70),
 
         width: double.infinity,
-        height: 200,
+
         child: Align(
           alignment: Alignment.centerLeft,
-          child: Column(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(Icons.arrow_back, color: Colors.white),
+          child: Container(
+            width: double.infinity,
+            // color: Colors.red,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  // color: Colors.blueAccent,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.arrow_back, color: Colors.white),
+                      ),
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 38,
+                          color: Colors.yellow,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        "We are happy to have back",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
                   ),
-                  Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 38,
-                      color: Colors.yellow,
-                      fontWeight: FontWeight.w600,
+                ),
+                SizedBox(height: 70),
+                Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        prefixIcon: Icon(Icons.email_outlined),
+                        iconColor: Colors.yellow,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "We are happy to have back",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
