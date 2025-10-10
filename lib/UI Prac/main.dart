3aeeb2e61462f50626ef583815abb193c651ee10
Ashir Ahmed <ashirahmed810@gmail.com>
2025-10-10 19:23:00 +0500

    UI Prac

--- lib/UI Prac/main.dart
+++ lib/UI Prac/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/UI%20Prac/Screens/Screen.dart';
+import 'package:todo_app/UI%20Prac/Screens/initscr.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
