5cd31a6ae71254d3532f938dfb3dd175fd18af3d
Ashir Ahmed <ashirahmed810@gmail.com>
2025-10-11 20:50:43 +0500

    UI

--- lib/UI Prac/Screens/Home.dart
+++ lib/UI Prac/Screens/Home.dart
@@ -12,47 +12,61 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            // height: 100,
-            width: double.infinity,
-            margin: EdgeInsets.only(top: 35),
-            child: Container(
-              color: Colors.red,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          children: [
+            Container(
+              // height: 100,
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 35),
+              child: Container(
+                color: Colors.red,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
 
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(
+                          context,
+                        ); // 👈 goes back to previous screen
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: const Color.fromARGB(255, 42, 39, 9),
+              child: Column(
                 children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
-                    onPressed: () {
-                      Navigator.pop(context); // 👈 goes back to previous screen
-                    },
+                  Text(
+                    "Admin Dashboard",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.yellow,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  Text(
+                    "Manage patients and appointments",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
                   ),
                 ],
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: const Color.fromARGB(255, 42, 39, 9),
-            child: Column(
-              children: [
-                Text(
-                  "Admin Dashboard",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.yellow,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
