1100ab313e8f0b7ac5ec6bb77f0602187920b1e6
Ashir Ahmed <ashirahmed810@gmail.com>
2025-10-13 20:23:33 +0500

    AddScrn Ui

--- lib/UI_Prac/Dashboard_Scrn/Add_Scr.dart
+++ lib/UI_Prac/Dashboard_Scrn/Add_Scr.dart
@@ -11,11 +11,56 @@ class _AddScrState extends State<AddScr> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Container(
-        color: Colors.red,
-        height: 300,
-        child: Column(children: []),
+        color: Colors.black,
+
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                color: Colors.red,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: const Color.fromARGB(255, 9, 131, 55),
+                      height: 100,
+                      width: 50,
+                      margin: EdgeInsets.only(top: 30),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Center(
+                      child: Image.asset(
+                        "lib/assets/logo.png",
+                        height: 200,
+                        width: 220,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Text("Add New patient"),
+          ],
+        ),
       ),
     );
   }
