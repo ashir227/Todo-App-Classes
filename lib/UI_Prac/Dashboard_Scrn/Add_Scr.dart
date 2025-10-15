fb8fbbc125b834c1c30a39e90a94bd30f7e844eb
Ashir Ahmed <ashirahmed810@gmail.com>
2025-10-15 20:29:17 +0500

    AddScrn Ui

--- lib/UI_Prac/Dashboard_Scrn/Add_Scr.dart
+++ lib/UI_Prac/Dashboard_Scrn/Add_Scr.dart
@@ -26,136 +26,155 @@ class _AddScrState extends State<AddScr> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 13),
-        color: Colors.black,
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 13),
+            color: Colors.black,
 
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 32),
-              // color: Colors.red,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-
-                  Image.asset("lib/assets/logo.png", height: 180, width: 200),
-                  SizedBox(width: 17),
-                ],
-              ),
-            ),
-
-            Column(
+            width: double.infinity,
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text.rich(
-                  TextSpan(
+                Container(
+                  margin: EdgeInsets.only(bottom: 32),
+                  // color: Colors.red,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      TextSpan(
-                        text: "Add ",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      TextSpan(
-                        text: "New ",
-                        style: TextStyle(
+                      IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
                           color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
+                          size: 30,
                         ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
                       ),
 
-                      TextSpan(
-                        text: "Patients",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      Image.asset(
+                        "lib/assets/logo.png",
+                        height: 180,
+                        width: 200,
                       ),
+                      SizedBox(width: 17),
                     ],
                   ),
                 ),
-                Text(
-                  "Enter patient details",
-                  style: TextStyle(color: Colors.white, fontSize: 18.5),
-                ),
-              ],
-            ),
-            SizedBox(height: 70),
-            Column(
-              children: [
-                newMethod(
-                  Icons.person_2_outlined,
-                  "Patient Name",
-                  _namenode,
-                  nameController,
-                ),
-                SizedBox(height: 20),
-                newMethod(
-                  Icons.calendar_month_outlined,
-                  "Date of Birth",
-                  _dobNode,
-                  dobController,
-                ),
-                SizedBox(height: 20),
-                newMethod(
-                  Icons.male_outlined,
-                  "Gender",
-                  _genderNode,
-                  genderController,
-                ),
-                SizedBox(height: 20),
-                newMethod(
-                  Icons.phone,
-                  "Contact Number",
-                  _contactNode,
-                  contactController,
-                ),
-                SizedBox(height: 20),
-                newMethod(
-                  Icons.message_outlined,
-                  "Disease / Remarks",
-                  _diseaseNode,
-                  diseaseController,
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Center(
-              child: Container(
-                width: 400,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(10),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Add ",
+                            style: TextStyle(
+                              color: Colors.yellow,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "New ",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          TextSpan(
+                            text: "Patients",
+                            style: TextStyle(
+                              color: Colors.yellow,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    backgroundColor: const Color.fromARGB(210, 227, 230, 150),
-                  ),
-                  onPressed: () {
-                    setState(() {});
-                  },
-                  child: Text(
-                    "Save Patient",
-                    style: TextStyle(fontSize: 22, color: Colors.black),
+                    Text(
+                      "Enter patient details",
+                      style: TextStyle(color: Colors.white, fontSize: 18.5),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 70),
+                Column(
+                  children: [
+                    newMethod(
+                      Icons.person_2_outlined,
+                      "Patient Name",
+                      _namenode,
+                      nameController,
+                    ),
+                    SizedBox(height: 20),
+                    newMethod(
+                      Icons.calendar_month_outlined,
+                      "Date of Birth",
+                      _dobNode,
+                      dobController,
+                    ),
+                    SizedBox(height: 20),
+                    newMethod(
+                      Icons.male_outlined,
+                      "Gender",
+                      _genderNode,
+                      genderController,
+                    ),
+                    SizedBox(height: 20),
+                    newMethod(
+                      Icons.phone,
+                      "Contact Number",
+                      _contactNode,
+                      contactController,
+                    ),
+                    SizedBox(height: 20),
+                    newMethod(
+                      Icons.message_outlined,
+                      "Disease / Remarks",
+                      _diseaseNode,
+                      diseaseController,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 50),
+                Center(
+                  child: Container(
+                    width: 400,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(10),
+                        ),
+                        backgroundColor: const Color.fromARGB(
+                          210,
+                          227,
+                          230,
+                          150,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: Text(
+                        "Save Patient",
+                        style: TextStyle(fontSize: 22, color: Colors.black),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -185,7 +204,10 @@ class _AddScrState extends State<AddScr> {
           borderSide: BorderSide(color: Colors.yellow, width: 2),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.black, width: 2),
+          borderSide: BorderSide(
+            color: const Color.fromARGB(255, 196, 184, 84),
+            width: 2,
+          ),
         ),
       ),
     );
